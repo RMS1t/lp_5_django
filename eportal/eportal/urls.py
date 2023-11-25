8d1a6e80e69548377f61ca2a7b40de1280dcf3cd
RMS1t <146536000+RMS1t@users.noreply.github.com>
2023-11-25 13:21:42 +0700

    Update urls.py

--- eportal/eportal/urls.py
+++ eportal/eportal/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('taskboard.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('taskboard.urls')),
+    path('accounts/profile/', RedirectView.as_view(url='my-orders/')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
